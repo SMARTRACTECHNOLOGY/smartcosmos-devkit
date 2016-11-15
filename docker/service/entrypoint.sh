3a2a627a94e0b05dc9430c5ed7c4aa36cd0748ca
Robert Van Voorhees <rcvanvo@gmail.com>
2016-11-15 17:22:26 -0500

    Changes the way we handle services, allowing you to bypass the need for the config server.

--- docker/service/entrypoint.sh
+++ docker/service/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-exec dockerize -timeout 5m -wait ${CONFIG_SERVER_URL}/admin/status java $JAVA_OPTS -jar $@
+if [ -z "$SPRING_CONFIG_LOCATION" ]; then
+  java $JAVA_OPTS -jar $@ $SERVER_PORT $SPRING_CONFIG_LOCATION $SPRING_PARAMETERS 
+else
+  exec dockerize -timeout 5m -wait ${CONFIG_SERVER_URL}/admin/status java $JAVA_OPTS -jar $@ $SERVER_PORT $SPRING_CONFIG_LOCATION $SPRING_PARAMETERS
+fi
