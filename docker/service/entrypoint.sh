a4080ef29ce4f255d0cdbcc58500ce87abe99f6b
Stan Domula <stan.domula@smartrac-group.com>
2016-12-06 15:02:39 +0100

    move the active spring profile into its own var

--- docker/service/entrypoint.sh
+++ docker/service/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 if [[ -z "${SPRING_CONFIG_LOCATION// }" ]]; then
-  exec dockerize -timeout 5m -wait ${CONFIG_SERVER_URL}/admin/status java $JAVA_OPTS -jar $@ $SERVER_PORT $SPRING_PARAMETERS
+  exec dockerize -timeout 5m -wait ${CONFIG_SERVER_URL}/admin/status \
+        java $JAVA_OPTS -Dspring.profiles.active=${SPRING_PROFILE} \
+        -jar $@ $SERVER_PORT $SPRING_PARAMETERS
 else
-  java $JAVA_OPTS -jar $@ $SERVER_PORT $SPRING_CONFIG_LOCATION $SPRING_PARAMETERS
+  java $JAVA_OPTS -Dspring.profiles.active=${SPRING_PROFILE} \
+        -jar $@ $SERVER_PORT $SPRING_CONFIG_LOCATION $SPRING_PARAMETERS
 fi
