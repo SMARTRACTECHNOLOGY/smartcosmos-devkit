d0e72218bce023afce07ffe6258e12e29f1e908d
Stan Domula <stan.domula@smartrac-group.com>
2016-12-06 16:14:06 +0100

    clean up

--- docker/service/entrypoint.sh
+++ docker/service/entrypoint.sh
@@ -5,7 +5,7 @@ if [[ -z "${SPRING_PROFILE}" ]]; then
   spring_profile_options="-Dspring.profiles.active=${SPRING_PROFILE}"
 fi
 
-if [[ -z "${SPRING_CONFIG_LOCATION// }" ]]; then
+if [[ -z "${SPRING_CONFIG_LOCATION}" ]]; then
   exec dockerize -timeout 5m -wait ${CONFIG_SERVER_URL}/admin/status \
         java $JAVA_OPTS ${spring_profile_options} \
         -jar $@ $SERVER_PORT $SPRING_PARAMETERS
