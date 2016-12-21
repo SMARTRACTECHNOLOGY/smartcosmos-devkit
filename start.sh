bd1dff6422dd80018d48abef478bb5a22230271a
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-12-21 14:00:00 +0100

    Apply Google's formatting guidelines for loops

--- start.sh
+++ start.sh
@@ -19,8 +19,7 @@ function http_get_succeeds() {
   local started=false
   local status=${UNDEFINED_STATUS}
 
-  while [ $started = false ] && [ $attempt -lt 10 ]
-  do
+  while [ $started = false ] && [ $attempt -lt 10 ]; do
     set +e
     status=$(curl -s -o /dev/null -w "%{http_code}" ${URL})
     set -e
@@ -33,8 +32,7 @@ function http_get_succeeds() {
     attempt=$((attempt + 1))
   done
 
-  if [ ${attempt} -eq ${MAX_ATTEMPTS} ] && [ ${started} = false ]
-  then
+  if [ ${attempt} -eq ${MAX_ATTEMPTS} ] && [ ${started} = false ]; then
     return 1
   else
     return 0
@@ -46,8 +44,7 @@ function container_is_up() {
   local container_count=$(docker-compose ps | grep -E "$NAME" | wc -l)
   local container_up_count=$(docker-compose ps | grep -E "$NAME" | grep "Up" | wc -l)
 
-  if [ ${container_count} -eq ${container_up_count} ]
-  then
+  if [ ${container_count} -eq ${container_up_count} ]; then
     return 0
   else
     return 1
@@ -59,8 +56,7 @@ function spinner() {
     local s='-\|/';
     local i=0;
 
-    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]
-    do
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
       i=$(( (i+1) %4 ))
       printf "\r${s:$i:1}"
       sleep .15
@@ -88,8 +84,7 @@ function configserver() {
 
   echo "Waiting for the server to become available..."
 
-  if http_get_succeeds ${CONFIG_SERVER_STATUS_URL}
-  then
+  if http_get_succeeds ${CONFIG_SERVER_STATUS_URL}; then
     echo "${green}✓${default_color} Done."
   else
     err "The config server did not respond. It may become available later, please check the log:\n$ docker-compose logs -f config-server"
@@ -99,22 +94,19 @@ function configserver() {
 function services() {
   echo "\n→ Starting user and auth services..."
   docker-compose up -d edge-user-devkit user-details-devkit auth-server
-  if ! container_is_up "edge-user-devkit|user-details-devkit|auth-server"
-  then
+  if ! container_is_up "edge-user-devkit|user-details-devkit|auth-server"; then
     sleep 60s & spinner
   fi
 
   echo "\n→ Starting core services..."
   docker-compose up -d ext-relationships ext-metadata ext-things edge-things
-  if ! container_is_up "ext-relationships|ext-metadata|ext-things|edge-things"
-  then
+  if ! container_is_up "ext-relationships|ext-metadata|ext-things|edge-things"; then
     sleep 90s & spinner
   fi
 
   echo "\n→ Starting event service..."
   docker-compose up -d events
-  if ! container_is_up "events"
-  then
+  if ! container_is_up "events"; then
     sleep 10s & spinner
   fi
 
@@ -142,8 +134,7 @@ function main() {
   docker ps
   echo "\nNote that it will take a few moments until they are available and can answer requests! 💡"
 
-  if http_get_succeeds "http://localhost:8080"
-  then
+  if http_get_succeeds "http://localhost:8080"; then
     echo "You're ready to take off now! Enjoy your SMART COSMOS adventures! 🚀"
   else
     err "The gateway did not respond. It may become available later, please check the log:\n$ docker-compose logs -f gateway"
